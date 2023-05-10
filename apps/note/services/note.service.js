d21583e38fbd09e906933762aa66a8b5ef6652b9
gertzMan <101058145+gertzMan@users.noreply.github.com>
2023-05-10 12:44:18 +0300

    initial service structure

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,33 @@
 // note service
+import { storageService } from '../../../services/async-storage.service.js'
+import { utilService } from '../../../services/util.service.js'
+
+export const noteService = {}
+
+const note = {
+  id: 'n101',
+  createdAt: 1112222,
+  type: 'NoteTxt',
+  isPinned: true,
+  style: { backgroundColor: '#00d' },
+  info: { txt: 'Fullstack Me Baby!' },
+}
+
+const loggedInUser = {
+  email: 'user@appsus.com',
+  fullName: 'Mahatma Appsus',
+}
+const filterBy = {
+  type: '',
+  txt: '',
+}
+
+function query(filterBy) {
+  // return notes according to searched params
+}
+
+function get(noteIx) {
+  // return storageService()
+}
+
+function remove() {}
