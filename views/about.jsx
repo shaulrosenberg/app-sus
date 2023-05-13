4e8a37b1e8d61d30a5c48bbb61185bbaa4624a82
gertzMan <101058145+gertzMan@users.noreply.github.com>
2023-05-13 15:13:03 +0300

    added about page

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,33 @@
-
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
+  return (
+    <section className="about">
+      <h2>About Us</h2>
+      <div className="content">
+        <div className="description">
+          <h3>Adam Gertzkin</h3>
+          <p>
+            My name is Adam Gertzkin and I'm one of the creators of this app.
+            I'm passionate about web development and have honed my skills during
+            a coding academy bootcamp. Working with my partner, Shaul Rosenberg,
+            we built this amazing React app and had a great time learning and
+            collaborating together.
+          </p>
+          <img src="../assets/img/shaul.svg" alt="" />
+        </div>
+        <div className="description">
+          <h3>Shaul Rosenberg</h3>
+          <p>
+            I'm Shaul Rosenberg, the other half of the development team behind
+            this app. Like Adam, I also participated in a coding academy
+            bootcamp where I discovered my passion for coding. Collaborating
+            with Adam was a fantastic experience, and together we created an
+            incredible React app that we're proud of.
+          </p>
+          <img src="../assets/img/adam.svg" alt="" />
+        </div>
+      </div>
     </section>
+  )
 }
+
+export default About
--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,31 @@
+const { Link, NavLink } = ReactRouterDOM
 
 export function Home() {
-    return (
-        <section className="home">
-            <h1>Welcome to home page!</h1>
-            <main className="hero-section">
+  return (
+    <section className="home">
+      <h1>Welcome to the Home page!</h1>
+      <main className="hero-section">
+        <h2>Explore and Manage</h2>
+        <p>Discover the power of our applications</p>
+      </main>
 
-            </main>
-
-            <section className="features-section">
-
-            </section>
-        </section>
-    )
+      <section className="features-section">
+        <div>
+          <h3>Mail App</h3>
+          <img src="../assets/img/mail-icon.png" alt="Mail Icon" />
+          <p>Manage your emails with ease</p>
+        </div>
+        <div>
+          <h3>Notes App</h3>
+          <img src="../assets/img/note.png" alt="Notes Icon" />
+          <p>Stay organized with notes and reminders</p>
+        </div>
+        <div>
+          <h3>Books App</h3>
+          <img src="../assets/img/book.png" alt="Books Icon" />
+          <p>Discover and track your favorite books</p>
+        </div>
+      </section>
+    </section>
+  )
 }
